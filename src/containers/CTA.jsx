3011dd346031ed7c7ba9e041f48054996801a89c
Abir Santra <santraabir8@gmail.com>
2022-10-29 19:29:09 +0530

    Designed CTA

---
+++ src/containers/CTA.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { HiArrowRight } from "react-icons/hi";
+
+const CTA = () => {
+	return (
+		<div className="cta w-full flex justify-center items-center py-28 bg-primary">
+			<div className="cta--container section flex justify-center items-center flex-col gap-8">
+				<h1 className="cta--headline text-center font-extrabold text-white-two text-[2.5rem]">
+					Build your resume now <br /> and get that dream job today!
+				</h1>
+				<button className="btn primary--btn--inverted">
+					Get Started now <HiArrowRight />
+				</button>
+				<p className="cta--subheading text-white-two font-light text-sm">
+					It's completely free. No credit card required.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default CTA;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../containers/Hero";
 import WhyChooseUs from "../containers/WhyChooseUs";
 import Steps from "../containers/Steps";
+import CTA from "../containers/CTA";
 
 const LandingPage = () => {
 	return (
@@ -9,6 +10,7 @@ const LandingPage = () => {
 			<Hero />
 			<WhyChooseUs />
 			<Steps />
+			<CTA />
 		</div>
 	);
 };

87aaed54b22de73fb2035cc28bc78b56989fb23f
Abir Santra <santraabir8@gmail.com>
2022-10-30 10:25:49 +0530

    minor changes

--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ReviewsPage = () => {
-	return <div>ReviewsPage</div>;
+	return <div className="min-h-screen">ReviewsPage</div>;
 };
 
 export default ReviewsPage;

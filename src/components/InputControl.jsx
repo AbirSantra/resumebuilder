95afadf9e71b1a1eabc495d2dcaa1d2739290b32
Abir Santra <santraabir8@gmail.com>
2022-11-21 19:27:38 +0530

    added hints

--- src/components/InputControl.jsx
+++ src/components/InputControl.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { FaQuestionCircle } from "react-icons/fa";
 
-const InputControl = ({ label, textarea, ...props }) => {
+const InputControl = ({ label, textarea, hint, ...props }) => {
 	return textarea ? (
 		<div className="w-full text-grey-three focus-within:text-primary  flex justify-center items-start flex-col gap-1 col-span-2">
-			<label className="text-sm  ml-1">{label}</label>
+			<div className="flex justify-center items-center gap-2">
+				<label className="text-sm  ml-1">{label}</label>
+				{hint ? (
+					<div className="relative">
+						<FaQuestionCircle className="peer cursor-pointer" />
+						<span className="absolute hidden peer-hover:flex top-0 left-8 w-[200px] bg-grey-one text-white-one text-sm rounded-lg p-4">
+							{hint}
+						</span>
+					</div>
+				) : null}
+			</div>
 			<div className=" w-full flex justify-center items-center px-4 py-3 rounded-md border border-grey-four focus-within:border-primary">
 				<textarea
 					className="w-full border-none outline-none bg-transparent text-black-two font-medium text-base"
@@ -13,7 +24,17 @@ const InputControl = ({ label, textarea, ...props }) => {
 		</div>
 	) : (
 		<div className="w-full text-grey-three focus-within:text-primary  flex justify-center items-start flex-col gap-1">
-			<label className="text-sm  ml-1">{label}</label>
+			<div className="flex justify-center items-center gap-2">
+				<label className="text-sm  ml-1">{label}</label>
+				{hint ? (
+					<div className="relative">
+						<FaQuestionCircle className="peer cursor-pointer" />
+						<span className="absolute hidden peer-hover:flex top-0 left-8 w-[200px] bg-grey-one text-white-one text-sm rounded-lg p-4">
+							{hint}
+						</span>
+					</div>
+				) : null}
+			</div>
 			<div className=" w-full flex justify-center items-center px-4 py-3 rounded-md border border-grey-four focus-within:border-primary">
 				<input
 					className="w-full border-none outline-none bg-transparent text-black-two font-medium text-base"

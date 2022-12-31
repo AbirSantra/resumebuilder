7d9d230790bd78c1ca818b70f0928eaf3a829c13
Abir Santra <santraabir8@gmail.com>
2022-12-31 21:18:42 +0530

    section redesign

--- src/containers/WhyChooseUs.jsx
+++ src/containers/WhyChooseUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Feature from "../components/Feature";
 import SectionHeader from "../components/SectionHeader";
-import freeIcon from "../images/flash.png";
-import easyIcon from "../images/check.png";
-import customIcon from "../images/equalizer.png";
-import downloadIcon from "../images/downloads.png";
+import {
+	MdMoneyOff,
+	MdOutlineDashboardCustomize,
+	MdShare,
+} from "react-icons/md";
+import { BsLightningCharge } from "react-icons/bs";
 
 const WhyChooseUs = () => {
 	return (
@@ -16,11 +18,27 @@ const WhyChooseUs = () => {
 						subheading="Building a resume can be frustrating. Let us do the hard part!"
 					/>
 				</div>
-				<div className="whychooseus--features w-full flex justify-evenly items-center gap-4">
-					<Feature icon={freeIcon} color="#e11d48" label="Completely Free" />
-					<Feature icon={easyIcon} color="#65a30d" label="Easy to Use" />
-					<Feature icon={customIcon} color="#0284c7" label="Customizable" />
-					<Feature icon={downloadIcon} color="#7c3aed" label="PDF Download" />
+				<div className="whychooseus--features mt-8 grid grid-cols-2 gap-x-16  gap-y-24">
+					<Feature
+						icon={<MdMoneyOff />}
+						label="Completely Free"
+						desc="Resu.me is and always will be 100% free. Create an account for free. No credit card required."
+					/>
+					<Feature
+						icon={<BsLightningCharge />}
+						label="Fast and Easy to Use"
+						desc="Resu.me makes creating a resume lightning fast. It is as easy as filling up a form and a few clicks!"
+					/>
+					<Feature
+						icon={<MdOutlineDashboardCustomize />}
+						label="Customizable"
+						desc="Customize your resume using our collection of templates and accents colors. Make your resume unique!"
+					/>
+					<Feature
+						icon={<MdShare />}
+						label="Shareable"
+						desc="Share your resume as an online page or download and export it as a pdf file. Resu.me gives you both options."
+					/>
 				</div>
 			</div>
 		</div>

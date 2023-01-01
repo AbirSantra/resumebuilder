1fa36a94553c8406542f1cfee8cf40d2b83216c5
Abir Santra <santraabir8@gmail.com>
2023-01-01 19:22:42 +0530

    increased headline font size

--- src/containers/CTA.jsx
+++ src/containers/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
 	return (
 		<div className="cta w-full flex justify-center items-center py-28 bg-primary">
 			<div className="cta--container section flex justify-center items-center flex-col gap-8">
-				<h1 className="cta--headline text-center font-extrabold text-white-two text-[2.5rem]">
+				<h1 className="cta--headline text-center font-extrabold text-white-two text-6xl">
 					Build your resume now <br /> and get that dream job today!
 				</h1>
 				<button className="btn primary--btn--inverted">

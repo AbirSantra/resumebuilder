6821c15eb45d8418c382a131528b3f9736e7285a
Abir Santra <santraabir8@gmail.com>
2023-01-04 19:50:41 +0530

    changed color theme

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -4,10 +4,10 @@ const Feature = ({ icon, desc, label }) => {
 	return (
 		<div className="group flex flex-col justify-center items-center gap-4">
 			<div
-				className="flex justify-center items-center w-[3rem] h-[3rem] rounded-xl bg-blue-600/20 p-4 group-hover:scale-125 ease-in-out duration-200"
+				className="flex justify-center items-center w-[3rem] h-[3rem] rounded-xl bg-primary/20 p-4 group-hover:scale-125 ease-in-out duration-200"
 				// style={{ backgroundColor: `${color}` }}
 			>
-				<span className="text-2xl text-blue-600">{icon}</span>
+				<span className="text-2xl text-primary">{icon}</span>
 			</div>
 			<p className="mt-2 font-semibold text-lg text-slate-800">{label}</p>
 			<p className="w-3/5 text-center text-base text-slate-500">{desc}</p>

5da3e6d17d54372c214c15945e120450f4b0e2f6
Abir Santra <santraabir8@gmail.com>
2023-01-10 21:22:13 +0530

    changed tip icon

--- src/components/InputControl.jsx
+++ src/components/InputControl.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaQuestionCircle } from "react-icons/fa";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 const InputControl = ({ label, textarea, hint, ...props }) => {
 	return textarea ? (
@@ -14,7 +14,7 @@ const InputControl = ({ label, textarea, hint, ...props }) => {
 				</label>
 				{hint ? (
 					<div className="relative">
-						<FaQuestionCircle className="peer cursor-pointer" />
+						<FaRegQuestionCircle className="peer cursor-pointer" />
 						<span className="absolute hidden z-10 peer-hover:flex top-4 left-6 w-[200px] bg-grey-one text-grey-four text-sm rounded-lg p-4">
 							{hint}
 						</span>
@@ -26,7 +26,7 @@ const InputControl = ({ label, textarea, hint, ...props }) => {
 		<div className="w-full text-grey-three focus-within:text-primary  flex justify-center items-start flex-col gap-1">
 			<div className="relative w-full flex justify-center items-center px-4 py-3 rounded-md border border-grey-four focus-within:border-primary ">
 				<input
-					className="peer w-full border-none outline-none bg-transparent font-light text-black-two"
+					className="peer w-full border-none outline-none bg-transparent font-light text-black-two placeholder:text-sm "
 					{...props}
 				/>
 				<label className="absolute left-1 -top-[12px] text-[13px] font-medium bg-white px-1 text-neutral-500 peer-focus:text-primary ml-1">
@@ -34,7 +34,7 @@ const InputControl = ({ label, textarea, hint, ...props }) => {
 				</label>
 				{hint ? (
 					<div className="relative">
-						<FaQuestionCircle className="peer cursor-pointer" />
+						<FaRegQuestionCircle className="peer cursor-pointer" />
 						<span className="absolute hidden z-10 peer-hover:flex top-4 left-6 w-[200px] bg-grey-one text-grey-four text-sm rounded-lg p-4">
 							{hint}
 						</span>

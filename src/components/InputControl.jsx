07c4ee3ac0d6fa80d28ad65d74f199ba55713950
Abir Santra <santraabir8@gmail.com>
2023-01-10 21:47:32 +0530

    combined textarea and input into single component

--- src/components/InputControl.jsx
+++ src/components/InputControl.jsx
@@ -1,37 +1,44 @@
 import React from "react";
+import { useState } from "react";
+import { BiHide, BiShowAlt } from "react-icons/bi";
 import { FaRegQuestionCircle } from "react-icons/fa";
 
-const InputControl = ({ label, textarea, hint, ...props }) => {
-	return textarea ? (
-		<div className="w-full text-grey-three focus-within:text-primary flex justify-center items-start flex-col gap-1 col-span-2">
-			<div className="relative w-full flex justify-center items-start px-4 py-3 rounded-md border border-grey-four focus-within:border-primary">
-				<textarea
-					className="peer w-full border-none outline-none bg-transparent font-light text-black-two"
-					{...props}
-				/>
-				<label className="absolute left-2 -top-[12px] text-[13px] font-medium bg-white px-1 text-neutral-500 peer-focus:text-primary ml-1">
-					{label}
-				</label>
-				{hint ? (
-					<div className="relative">
-						<FaRegQuestionCircle className="peer cursor-pointer" />
-						<span className="absolute hidden z-10 peer-hover:flex top-4 left-6 w-[200px] bg-grey-one text-grey-four text-sm rounded-lg p-4">
-							{hint}
-						</span>
-					</div>
-				) : null}
-			</div>
-		</div>
-	) : (
-		<div className="w-full text-grey-three focus-within:text-primary  flex justify-center items-start flex-col gap-1">
+const InputControl = ({ label, textarea, hint, password, type, ...props }) => {
+	// to store the show/hide password state
+	const [showPass, setShowPass] = useState(false);
+
+	//! to toggle show/hide password state
+	const toggleShowPass = () => {
+		setShowPass((prev) => !prev);
+	};
+
+	return (
+		<div
+			className={
+				"w-full text-grey-three focus-within:text-primary  flex justify-center items-start flex-col gap-1"
+			}
+		>
 			<div className="relative w-full flex justify-center items-center px-4 py-3 rounded-md border border-grey-four focus-within:border-primary ">
-				<input
-					className="peer w-full border-none outline-none bg-transparent font-light text-black-two placeholder:text-sm "
-					{...props}
-				/>
+				{/* Input Area */}
+				{textarea ? (
+					<textarea
+						className="peer w-full border-none outline-none bg-transparent font-light text-black-two"
+						{...props}
+					/>
+				) : (
+					<input
+						className="peer w-full border-none outline-none bg-transparent font-light text-black-two placeholder:text-sm "
+						type={showPass ? "text" : "password"}
+						{...props}
+					/>
+				)}
+
+				{/* Input Label */}
 				<label className="absolute left-1 -top-[12px] text-[13px] font-medium bg-white px-1 text-neutral-500 peer-focus:text-primary ml-1">
 					{label}
 				</label>
+
+				{/* Hint ToolTip */}
 				{hint ? (
 					<div className="relative">
 						<FaRegQuestionCircle className="peer cursor-pointer" />

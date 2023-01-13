8dceb1ff5379438c518612d2cec5beeaf63237cb
Abir Santra <santraabir8@gmail.com>
2023-01-13 20:35:15 +0530

    increased width

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Divider = () => {
-	return <div className="w-full h-[1px] min-h-[1px] bg-grey-four"></div>;
+	return <div className="w-full h-[2px] min-h-[2px] bg-grey-four"></div>;
 };
 
 export default Divider;

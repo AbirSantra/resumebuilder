0ac879777c489cd77fb43db325530d7c47bc671e
Abir Santra <santraabir8@gmail.com>
2023-01-13 21:23:35 +0530

    redesigned resume preview

--- src/containers/Resume.jsx
+++ src/containers/Resume.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-// import "./Resume.css";
+import { MdLocationPin } from "react-icons/md";
+import { FaPhoneAlt, FaEnvelope, FaLink, FaLinkedin } from "react-icons/fa";
+import Divider from "../components/Divider";
 
 const Resume = () => {
 	// get the resume data state
@@ -13,67 +15,79 @@ const Resume = () => {
 				id="resume"
 			>
 				{/* Header */}
-				<div className="header--container w-full flex justify-between items-center">
-					{/* Name and Profession */}
-					<div className="header flex flex-col">
+				<div className="header flex flex-col gap-4">
+					<div>
 						<h1 className="name font-bold text-2xl">
 							{data.header.firstname} {data.header.lastname}
 						</h1>
-						<p className="text-xs text-grey-three">{data.header.profession}</p>
+						<p className="text-sm text-primary">{data.header.profession}</p>
 					</div>
-
 					{/* Contact */}
-					<div className="contact--container text-[0.6rem] text-grey-two font-bold flex flex-col items-end">
-						<p className="contact flex gap-2">
-							Address:
-							<span className="font-normal">
+					<div className="contact--container font-bold w-full flex flex-wrap gap-y-2 gap-4 text-[0.6rem] text-primary">
+						<p className="contact flex justify-center items-center gap-2">
+							<MdLocationPin />
+							<span className="font-medium text-grey-one">
 								{data.header.city}, {data.header.country} -{data.header.pincode}
 							</span>
 						</p>
-						<p className="contact flex gap-2">
-							Phone:
-							<span className="font-normal">{data.header.phone}</span>
+						<p className="contact flex justify-center items-center gap-2">
+							<FaPhoneAlt />
+							<span className="font-medium text-grey-one">
+								{data.header.phone}
+							</span>
 						</p>
-						<p className="contact flex gap-2">
-							Email:
-							<span className="font-normal">{data.header.email}</span>
+						<p className="contact flex justify-center items-center gap-2">
+							<FaEnvelope />
+							<span className="font-medium text-grey-one">
+								{data.header.email}
+							</span>
 						</p>
-						<p className="contact flex gap-2">
-							Website:
-							<span className="font-normal">{data.header.website}</span>
+						<p className="contact flex justify-center items-center gap-2">
+							<FaLink />
+							<span className="font-medium text-grey-one">
+								{data.header.website}
+							</span>
 						</p>
-						<p className="contact flex gap-2">
-							Linkedin Profile:
-							<span className="font-normal">{data.header.linkedin}</span>
+						<p className="contact flex justify-center items-center gap-2">
+							<FaLinkedin />
+							<span className="font-medium text-grey-one">
+								{data.header.linkedin}
+							</span>
 						</p>
 					</div>
 				</div>
 
 				{/* Skills */}
 				<div className="container w-full flex flex-col justify-start items-start gap-1">
-					<div className="section--title font-bold text-grey-three">Skills</div>
-					<div className="divider w-full h-[1px] bg-grey-four"></div>
-					<div className="skills--content mt-1 text-[0.7rem] flex justify-start items-start gap-2">
+					<div className="section--title font-bold text-primary uppercase font-Ubuntu text-xs">
+						Skills
+					</div>
+					<Divider />
+					<div className="skills--content mt-1 text-[0.7rem] flex justify-start items-start flex-wrap gap-2">
 						{data.skills.map((skill) => (
-							<div className="skill--item">{skill}</div>
+							<div className="skill--item bg-primary-light px-2 font-semibold rounded-md text-white font-Ubuntu">
+								{skill}
+							</div>
 						))}
 					</div>
 				</div>
 
 				{/* Experience */}
 				<div className="w-full flex flex-col justify-start items-start gap-1">
-					<div className="font-bold text-grey-three">Experience</div>
-					<div className="divider w-full h-[1px] bg-grey-four"></div>
+					<div className="section--title font-bold text-primary uppercase font-Ubuntu text-xs">
+						Experience
+					</div>
+					<Divider />
 					<div className="mt-1 text-[0.7rem] flex flex-col justify-start items-start gap-2">
 						{data.experience.map((item) => (
 							<div className="w-full flex flex-col justify-start items-start">
 								<div className="font-bold text-xs">
 									{item.position}, {item.employer}
 								</div>
-								<div className="text-grey-three text-[0.6rem] font-semibold">
-									{item.startdate} to {item.enddate}
+								<div className="text-primary text-[0.6rem] font-semibold font-Ubuntu">
+									({item.startdate} to {item.enddate})
 								</div>
-								<div className="experience--description text-[0.6rem]">
+								<div className="experience--description text-[0.6rem] font-Ubuntu text-grey-three">
 									{item.description}
 								</div>
 							</div>
@@ -83,8 +97,10 @@ const Resume = () => {
 
 				{/* Projects */}
 				<div className="w-full flex flex-col justify-start items-start gap-1">
-					<div className="font-bold text-grey-three">Projects</div>
-					<div className="divider w-full h-[1px] bg-grey-four"></div>
+					<div className="section--title font-bold text-primary uppercase font-Ubuntu text-xs">
+						Projects
+					</div>
+					<Divider />
 					<div className="mt-1 text-[0.7rem] flex flex-col justify-start items-start gap-2">
 						{data.projects.map((item) => (
 							<div className="w-full flex flex-col justify-start items-start">
@@ -94,10 +110,10 @@ const Resume = () => {
 										Link
 									</span>
 								</div>
-								<div className="text-grey-three text-[0.6rem] font-semibold">
-									{item.startdate} to {item.enddate}
+								<div className="text-primary text-[0.6rem] font-semibold font-Ubuntu">
+									({item.startdate} to {item.enddate})
 								</div>
-								<div className="experience--description text-[0.6rem]">
+								<div className="experience--description text-[0.6rem] font-Ubuntu text-grey-three">
 									{item.description}
 								</div>
 							</div>
@@ -107,22 +123,24 @@ const Resume = () => {
 
 				{/* Education */}
 				<div className="w-full flex flex-col justify-start items-start gap-1">
-					<div className="font-bold text-grey-three">Education</div>
-					<div className="divider w-full h-[1px] bg-grey-four"></div>
+					<div className="section--title font-bold text-primary uppercase font-Ubuntu text-xs">
+						Education
+					</div>
+					<Divider />
 					<div className="mt-1 text-[0.7rem] flex flex-col justify-start items-start gap-2">
 						{data.education.map((item) => (
 							<div className="w-full flex flex-col justify-start items-start">
 								<div className="font-bold text-xs flex justify-center items-center gap-4">
 									{item.institute}{" "}
-									<div className="text-grey-three text-[0.6rem] font-semibold">
-										{item.startdate} to {item.enddate}
+									<div className="text-grey-three text-[0.6rem] font-semibold font-Ubuntu">
+										({item.startdate} to {item.enddate})
 									</div>
 								</div>
-								<span className="font-semibold text-grey-three text-[0.6rem]">
+								<span className="font-semibold text-primary text-[0.6rem]">
 									{item.degree}
 								</span>
 
-								<div className="experience--description text-[0.6rem]">
+								<div className="experience--description text-[0.6rem] font-Ubuntu">
 									Score: {item.score}
 								</div>
 							</div>
@@ -132,7 +150,9 @@ const Resume = () => {
 
 				{/* Certifications */}
 				<div className="w-full flex flex-col justify-start items-start gap-1">
-					<div className="font-bold text-grey-three">Certifications</div>
+					<div className="section--title font-bold text-primary uppercase font-Ubuntu text-xs">
+						Certifications
+					</div>
 					<div className="divider w-full h-[1px] bg-grey-four"></div>
 					<div className="mt-1 text-[0.7rem] flex flex-col justify-start items-start gap-2">
 						{data.certifications.map((item) => (
@@ -140,15 +160,13 @@ const Resume = () => {
 								<div className="font-bold text-xs flex justify-center items-center gap-4">
 									{item.certificationname}{" "}
 								</div>
-								<div className="text-grey-three text-[0.6rem] font-semibold flex gap-4">
+								<div className="text-primary text-[0.6rem] font-semibold flex gap-4">
 									Issuer: {item.organization}{" "}
-									<span className="font-semibold text-grey-three text-[0.6rem]">
-										Issue Date: {item.date}
-									</span>
+									<span>Issued On: {item.date}</span>
 								</div>
 
-								<div className="experience--description text-[0.6rem]">
-									Link: {item.url}
+								<div className="experience--description text-[0.6rem] font-Ubuntu">
+									Certificate Link: {item.url}
 								</div>
 							</div>
 						))}

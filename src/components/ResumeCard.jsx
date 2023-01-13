966502b4da7663d42380597234508b52397fd129
Abir Santra <santraabir8@gmail.com>
2023-01-13 18:38:03 +0530

    added share icon & added resume page link to resume name

--- src/components/ResumeCard.jsx
+++ src/components/ResumeCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiDownload, FiEdit, FiTrash2 } from "react-icons/fi";
+import { FiEdit, FiTrash2, FiShare2 } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useDeleteResumeMutation } from "../api/resumeApiSlice";
@@ -12,7 +12,7 @@ const ResumeCard = ({ resume }) => {
 	const currentUserId = useSelector((state) => state.auth.user._id);
 
 	// action for deletion
-	const [deleteResume, { isLoading: isDeleting }] = useDeleteResumeMutation();
+	const [deleteResume] = useDeleteResumeMutation();
 
 	//! Handle deletion
 	const handleDelete = async () => {
@@ -25,19 +25,28 @@ const ResumeCard = ({ resume }) => {
 
 	return (
 		<div className="resume--card w-full aspect-[3/4] group flex flex-col justify-center items-center px-6 py-6 rounded-lg border border-grey-four hover:border-primary  ease-in-out duration-300">
-			<h2 className="resume--name mt-8 text-lg font-semibold text-center text-grey-three group-hover:text-primary duration-300 ease-in-out">
+			<Link
+				to={`/resume/${resume._id}`}
+				className="resume--name mt-8 text-lg font-semibold text-center text-grey-three group-hover:text-primary group-hover:underline duration-300 ease-in-out"
+			>
 				{resume.name}
-			</h2>
+			</Link>
 			<p className="resume--updateTime text-center text-[12px] font-medium text-grey-three">
 				Updated {moment(resume.createdAt).fromNow()}
 			</p>
-			<div className="resume--options mt-4 flex justify-center items-center gap-8">
+			<div className="resume--options mt-4 flex justify-center items-center gap-4">
 				<Link
 					to={`/editor/${resume._id}`}
 					className="resume--options--icon group cursor-pointer invisible opacity-0 group-hover:visible group-hover:opacity-100 flex justify-center items-center gap-2 rounded-full p-3 text-grey-three hover:text-white bg-white hover:bg-primary duration-200 ease-in-out"
 				>
 					<FiEdit size={20} />
 				</Link>
+				<div
+					className="resume--options--icon group cursor-pointer invisible opacity-0 group-hover:visible group-hover:opacity-100 flex justify-center items-center gap-2 rounded-full p-3 text-grey-three hover:text-white bg-white hover:bg-primary duration-200 ease-in-out"
+					onClick={handleDelete}
+				>
+					<FiShare2 size={20} />
+				</div>
 				<div
 					className="resume--options--icon group cursor-pointer invisible opacity-0 group-hover:visible group-hover:opacity-100 flex justify-center items-center gap-2 rounded-full p-3 text-grey-three hover:text-white bg-white hover:bg-primary duration-200 ease-in-out"
 					onClick={handleDelete}

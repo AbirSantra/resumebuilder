aa45a6586ff8255fcde71d8214540c927dbeb9f0
Abir Santra <santraabir8@gmail.com>
2023-01-18 11:26:28 +0530

    changed design

--- src/components/FormSectionHeader.jsx
+++ src/components/FormSectionHeader.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const FormSectionHeader = ({ icon, title, subtitle }) => {
 	return (
-		<div className="header--heading w-full flex flex-col justify-start items-start ">
-			<div className="flex justify-center items-center gap-2 text-lg text-primary">
+		<div className="header--heading w-full flex flex-col justify-start items-start gap-2">
+			<div className="flex justify-center items-center gap-4 text-lg text-primary">
 				<span>{icon}</span>
 				<h1 className="header--title font-semibold text-2xl text-primary">
 					{title}
 				</h1>
 			</div>
-			<p className="header--subheading text-sm text-grey-three">{subtitle}</p>
+			<p className="header--subheading font-Ubuntu text-sm text-grey-three">
+				{subtitle}
+			</p>
 		</div>
 	);
 };

8d835905b06c4f50abb003884108c628f51a97e6
Abir Santra <santraabir8@gmail.com>
2023-01-20 20:42:21 +0530

    added scrolling feature

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -15,6 +15,7 @@ import Divider from "../components/Divider";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { resetResumeData, setResumeData } from "../redux/resumeSlice";
+import { useRef } from "react";
 
 const Editor = () => {
 	const dispatch = useDispatch();
@@ -31,7 +32,7 @@ const Editor = () => {
 		skip: isNew,
 	});
 
-	// set resume data into global state
+	//! set resume data into global state
 	useEffect(() => {
 		if (data) {
 			dispatch(setResumeData(data));
@@ -45,28 +46,103 @@ const Editor = () => {
 	// to store the current step
 	const [step, setStep] = useState(1);
 
+	// refs to form sections
+	const formBodyRef = useRef();
+	const headerRef = useRef();
+	const educationRef = useRef();
+	const skillsRef = useRef();
+	const experienceRef = useRef();
+	const projectsRef = useRef();
+	const certificationsRef = useRef();
+	const extraRef = useRef();
+	const finalizeRef = useRef();
+
+	//! to scroll to a particular section
+	const scrollToSection = (ref) => {
+		formBodyRef.current.scrollTo({
+			top: ref.current.offsetTop - 48,
+			// 48 because gap from the top form div is 48px or 5rem
+			behavior: "smooth",
+		});
+	};
+
+	//! to scroll whenver the step number changes
+	useEffect(() => {
+		const scrollChanger = () => {
+			switch (step) {
+				case 1:
+					scrollToSection(headerRef);
+					break;
+				case 2:
+					scrollToSection(educationRef);
+					break;
+				case 3:
+					scrollToSection(skillsRef);
+					break;
+				case 4:
+					scrollToSection(experienceRef);
+					break;
+				case 5:
+					scrollToSection(projectsRef);
+					break;
+				case 6:
+					scrollToSection(certificationsRef);
+					break;
+				case 7:
+					scrollToSection(extraRef);
+					break;
+				case 8:
+					scrollToSection(finalizeRef);
+					break;
+				default:
+					break;
+			}
+		};
+		scrollChanger();
+	}, [step]);
+
 	return (
 		<div className="editor w-full flex justify-center items-center">
 			<div className="editor--container w-full h-[calc(100vh-5rem)] grid grid-cols-[600px_auto]">
 				{/* Form */}
-				<div className="relative flex flex-col overflow-auto">
+				<div className="flex flex-col overflow-auto">
 					<FormSwitcher currentStep={step} setStep={setStep} />
 					<Divider />
-					<div className="section--padding py-12 w-full flex flex-col gap-12 overflow-auto">
-						<Header />
+					<div
+						className="relative section--padding py-12 w-full flex flex-col gap-12 overflow-auto"
+						ref={formBodyRef}
+					>
+						<div ref={headerRef}>
+							<Header />
+						</div>
 						<Divider />
-						<Education />
+						<div ref={educationRef}>
+							<Education />
+						</div>
 						<Divider />
-						<Skills />
+						<div ref={skillsRef}>
+							<Skills />
+						</div>
 						<Divider />
-						<Experience />
+						<div ref={experienceRef}>
+							<Experience />
+						</div>
 						<Divider />
-						<Projects />
+						<div ref={projectsRef}>
+							<Projects />
+						</div>
 						<Divider />
-						<Certifications />
+						<div ref={certificationsRef}>
+							<Certifications />
+						</div>
 						<Divider />
-						<Extra />
-						<Finalize />
+						<div ref={extraRef}>
+							<Extra />
+						</div>
+						<Divider />
+						<div ref={finalizeRef}>
+							<Finalize />
+						</div>
 					</div>
 				</div>
 				{/* Preview */}

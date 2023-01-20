97c2d03cdb48a946932a81472ddbfd6f1b14cb13
Abir Santra <santraabir8@gmail.com>
2023-01-20 20:42:41 +0530

    removed active state color

--- src/components/FormSwitcher.jsx
+++ src/components/FormSwitcher.jsx
@@ -14,81 +14,49 @@ const FormSwitcher = ({ setStep, currentStep }) => {
 	return (
 		<div className="w-full h-20 section--padding py-6 flex justify-between items-center">
 			<div
-				className={
-					currentStep === 1
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(1)}
 			>
 				<FaUserAlt />
 			</div>
 			<div
-				className={
-					currentStep === 2
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(2)}
 			>
 				<FaUserGraduate />
 			</div>
 			<div
-				className={
-					currentStep === 3
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(3)}
 			>
 				<FaLayerGroup />
 			</div>
 			<div
-				className={
-					currentStep === 4
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(4)}
 			>
 				<FaBriefcase />
 			</div>
 			<div
-				className={
-					currentStep === 5
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(5)}
 			>
 				<FaRocket />
 			</div>
 			<div
-				className={
-					currentStep === 6
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(6)}
 			>
 				<GoVerified />
 			</div>
 			<div
-				className={
-					currentStep === 7
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(7)}
 			>
 				<FaShapes />
 			</div>
 			<div
-				className={
-					currentStep === 8
-						? "section--icon cursor-pointer text-lg text-primary"
-						: "section--icon cursor-pointer text-lg text-grey-four"
-				}
+				className="section--icon cursor-pointer text-lg text-grey-four hover:text-primary duration-200 ease-in-out"
 				onClick={() => setStep(8)}
 			>
 				<FaPalette />
